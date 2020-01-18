018fcc5ef20e2d30533c70a3df61438f8f4e60a7
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2020-01-18 20:04:13 +0100

    recarga page

--- Conversions/wwwroot/js/site.js
+++ Conversions/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    //E 10 segundos (10000 milisegundos) se ejecutará la función refrescar
+    setTimeout(refrescar, 10000);
+});
+function refrescar() {
+    //Actualiza la página
+    location.reload();
+}
